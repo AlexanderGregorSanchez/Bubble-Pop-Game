afb20b9bd0070646786f3524344345b5d70c7afc
Alex Sanchez <crewcutalex@gmail.com>
2024-11-08 20:52:50 +0800

    Fixed script name issue

--- Assets/Scripts/LauncherBubblePreviewHandler.cs
+++ Assets/Scripts/LauncherBubblePreviewHandler.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class launcherBubblePreviewHandler : MonoBehaviour
+public class LauncherBubblePreviewHandler : MonoBehaviour
 {
     
     public BubblePopGameMgr bubblePopGameMgr;
