fb37deeb98b07fe06ef9729e8d3ecd4741db84ca
Alex Sanchez <crewcutalex@gmail.com>
2024-12-06 16:24:51 +0800

    Added script for debugging in inspector

---
+++ Assets/Scripts/DebugMessage.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugMessage : MonoBehaviour
+{
+    public void PrintDebugMessage(string message)
+    {
+        Debug.Log(message);
+    }
+}
